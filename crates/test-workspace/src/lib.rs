da04179275d36eb340e37b53b9be1f81b2426410
westshgit <west.sh.mail@gmail.com>
2025-06-16 15:58:22 +0100

    initial commit

---
+++ crates/test-workspace/src/lib.rs
@@ -0,0 +1,13 @@
+pub use test_lib::add;
+pub use test_lib_2::add as second_add;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
